cc774e4c8bd2e3a000b32d17a31692bf6cf77309
Shubham <androshubhu@gmail.com>
2016-09-18 00:47:51 +0530

    Implement modules in file calculate size of file

--- File.py
+++ File.py
@@ -1,21 +1,27 @@
 import os
-file_name = input("Enter file name ")
-if '.txt' in file_name:
-    file = open(file_name)
-    y = list()
-    z = list()
-    #size = os.path.getsize(file)
-    #print(size.st_size)
-    #print("@"*40)
+from stat import *
+
+def checkSize(file_name):
+    if os.stat(file_name).st_size < 250000:
+        return True
+    return False
+
+def line(file_name):
     s = ""
-    #print(file.read())
+    file = open(file_name)
     for x in file.readlines():
         s = s+x.rstrip('\n')
-        #s= s+x
-        z.append(x.split(' '))
-        #print("x:",s)
-    #print(y, type(y), len(y))
-    #s = s.replace("\n"," ")
-    y=s.split('.  ')
-    print(y,len(z))
+        z.append(x)
     file.close()
+    return s,z
+
+file_name = input("Enter file name ")
+if '.txt' in file_name and   checkSize(file_name):
+    y = list()
+    z = list()
+    s,z=line(file_name)
+    y=s.split('.  ')
+    print("Count of statement :- ",len(z))
+    print(y,len(z))
+else:
+    print("file is not present or of size more than 2mb")
