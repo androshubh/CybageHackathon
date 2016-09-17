e41b22e6acce9deac675c947e51ece99f1012bd6
Shubham <androshubhu@gmail.com>
2016-09-17 23:15:52 +0530

    Update file \n problem

--- File.py
+++ File.py
@@ -1,15 +1,21 @@
+import os
 file_name = input("Enter file name ")
 if '.txt' in file_name:
     file = open(file_name)
-    y = []
-    s = "abc"
+    y = list()
+    z = list()
+    #size = os.path.getsize(file)
+    #print(size.st_size)
+    #print("@"*40)
+    s = ""
     #print(file.read())
     for x in file.readlines():
-        s= s+x
-        #y.append(x.split(' '))
-        print("x:",s)
+        s = s+x.rstrip('\n')
+        #s= s+x
+        z.append(x.split(' '))
+        #print("x:",s)
     #print(y, type(y), len(y))
-    s.replace("\n","")
-    y.append(s.split('.'))
-    print(y,type(y))
+    #s = s.replace("\n"," ")
+    y=s.split('.  ')
+    print(y,len(z))
     file.close()
